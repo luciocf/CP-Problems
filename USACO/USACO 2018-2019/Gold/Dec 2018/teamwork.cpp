be1920304858367f5847941021a7328f0ede8d56
Lúcio Cardoso <lucio.code@gmail.com>
2019-06-20 18:04:18 -0300

    Added teamwork

---
+++ USACO/USACO 2018-2019/Gold/Dec 2018/teamwork.cpp
@@ -0,0 +1,38 @@
+// USACO Gold Dec 2018 - Teamwork
+// Lúcio Cardoso
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 1e4+10;
+
+int n, k;
+int a[maxn];
+
+int dp[maxn];
+
+int main(void)
+{
+	FILE *fin = fopen("teamwork.in", "r");
+	FILE *fout = fopen("teamwork.out", "w");
+
+	fscanf(fin, "%d %d", &n, &k);
+
+	for (int i = 1; i <= n; i++)
+		fscanf(fin, "%d", &a[i]);
+
+	for (int i = 1; i <= n; i++)
+	{
+		int mx = a[i];
+
+		for (int j = i; j >= max(1, i-k+1); j--)
+		{
+			mx = max(mx, a[j]);
+
+			dp[i] = max(dp[i], mx*(i-j+1) + dp[j-1]);
+		}
+	}
+
+	fprintf(fout, "%d\n", dp[n]);
+}
