aa24fcddfd8b08557920e2c2a529aa3aa8f9838d
Lúcio Cardoso <lucio.code@gmail.com>
2019-10-24 15:24:30 -0300

    Added pcb

--- BOI/BOI 2010/pcb.cpp
+++ BOI/BOI 2010/pcb.cpp
@@ -5,46 +5,11 @@
 
 using namespace std;
 
-const int maxn = 1e6+10;
+const int maxn = 1e5+10;
 
 typedef pair<int, int> pii;
 
 pii a[maxn];
-
-int last[maxn];
-int ans[maxn];
-
-int tree[4*maxn];
-
-void upd(int node, int l, int r, int pos, int v)
-{
-	if (l == r)
-	{
-		tree[node] = v;
-		return;
-	}
-
-	int mid = (l+r)>>1;
-
-	if (pos <= mid) upd(2*node, l, mid, pos, v);
-	else upd(2*node+1, mid+1, r, pos, v);
-
-	tree[node] = min(tree[2*node], tree[2*node+1]);
-}
-
-int query(int node, int l, int r, int x)
-{
-	if (l == r)
-	{
-		if (tree[node] <= x) return l;
-		return -1;
-	}
-
-	int mid = (l+r)>>1;
-
-	if (tree[2*node] <= x) return query(2*node, l, mid, x);
-	return query(2*node+1, mid+1, r, x);
-}
 
 int main(void)
 {
@@ -56,17 +21,17 @@ int main(void)
 
 	sort(a+1, a+n+1);
 
+	multiset<int> st;
+
 	for (int i = 1; i <= n; i++)
 	{
-		int mex = query(1, 1, n, a[i].second);
+		auto it = st.lower_bound(a[i].second);
 
-		ans[i] = mex, last[mex] = max(last[mex], a[i].second);
-		upd(1, 1, n, mex, last[mex]);
+		if (it != st.begin())
+			st.erase(--it);
+
+		st.insert(a[i].second);
 	}
 
-	int ansF = 0;
-	for (int i = 1; i <= n; i++)
-		ansF = max(ansF, ans[i]);
-
-	printf("%d\n", ansF);
+	printf("%d\n", st.size());
 }
