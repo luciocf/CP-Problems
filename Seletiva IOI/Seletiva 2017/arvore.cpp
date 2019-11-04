b1cd2aab6887501b6453d0ba8046cc1937820aab
Lúcio Cardoso <lucio.code@gmail.com>
2019-11-04 15:12:56 -0300

    Added arvore

--- Seletiva IOI/Seletiva 2017/arvore.cpp
+++ Seletiva IOI/Seletiva 2017/arvore.cpp
@@ -7,48 +7,26 @@ using namespace std;
 
 const int maxn = 5e5+10;
 
-const int maxl = 20;
-
 struct Node
 {
-	Node *l, *r;
 	int v;
+	Node *l, *r;
 
 	Node()
 	{
-		l = r = NULL;
 		v = 0;
+		l = r = NULL;
 	}
 };
 
-Node *version[maxn];
+int n, q;
+int a[maxn];
 
-int num[maxn], ind[maxn], n, V;
-
-int tab[maxn][maxl], pai[maxn], nivel[maxn];
-
-map<int, int> back;
+int nivel[maxn], tab[maxn][21];
 
 vector<int> grafo[maxn];
 
-void compress(void)
-{
-	int aux = 0;
-	map<int, int> M;
-	vector<int> v;
-
-	for (int i = 1; i <= n; i++)
-		v.push_back(num[i]);
-
-	sort(v.begin(), v.end());
-
-	for (int i = 0; i < v.size(); i++)
-		if (M.find(v[i]) == M.end())
-			M[v[i]] = ++aux, back[aux] = v[i];
-
-	for (int i = 1; i <= n; i++)
-		num[i] = M[num[i]];
-}
+Node *version[maxn];
 
 void build(Node *node, int l, int r)
 {
@@ -61,7 +39,7 @@ void build(Node *node, int l, int r)
 	build(node->l, l, mid); build(node->r, mid+1, r);
 }
 
-void upd(Node *prev, Node *node, int l, int r, int pos)
+void upd(Node *prev, Node *node, int l, int r, int pos, int v)
 {
 	if (l == r)
 	{
@@ -74,113 +52,107 @@ void upd(Node *prev, Node *node, int l, int r, int pos)
 	if (pos <= mid)
 	{
 		node->r = prev->r;
-		if (node->l == NULL) node->l = new Node();
+		if (!node->l) node->l = new Node();
 
-		upd(prev->l, node->l, l, mid, pos);
+		upd(prev->l, node->l, l, mid, pos, v);
 	}
 	else
 	{
 		node->l = prev->l;
-		if (node->r == NULL) node->r = new Node();
+		if (!node->r) node->r = new Node();
 
-		upd(prev->r, node->r, mid+1, r, pos);
+		upd(prev->r, node->r, mid+1, r, pos, v);
 	}
 
 	node->v = node->l->v+node->r->v;
 }
 
-int kth(Node *node1, Node *node2, Node *node3, int l, int r, int k, int num_lca)
+int kth(Node *node1, Node *node2, Node *node3, int l, int r, int k, int num)
 {
 	if (l == r) return l;
 
 	int mid = (l+r)>>1;
-	int x = node1->l->v+node2->l->v-2*node3->l->v;
 
-	if (num_lca >= l && num_lca <= mid) x--;
+	int qtd = node1->l->v + node2->l->v - 2*node3->l->v + (num >= l && num <= mid);
 
-	if (x >= k) return kth(node1->l, node2->l, node3->l, l, mid, k, num_lca);
-	return kth(node1->r, node2->r, node3->r, mid+1, r, k-x, num_lca);
+	if (qtd >= k) return kth(node1->l, node2->l, node3->l, l, mid, k, num);
+	return kth(node1->r, node2->r, node3->r, mid+1, r, k-qtd, num);
 }
 
 void dfs(int u, int p)
 {
-	ind[u] = ++V;
-	version[ind[u]] = new Node();
-
-	upd(version[ind[p]], version[ind[u]], 1, n, num[u]);
-
 	for (auto v: grafo[u])
 	{
 		if (v == p) continue;
 
-		pai[v] = u, nivel[v] = nivel[u]+1;
-
+		nivel[v] = nivel[u]+1, tab[v][0] = u;
 		dfs(v, u);
 	}
 }
 
-void build_lca(void)
+void build_tab(void)
 {
-	memset(tab, -1, sizeof tab);
-
-	for (int i = 1; i <= n; i++)
-		tab[i][0] = pai[i];
-
-	for (int j = 1; j < maxl; j++)
+	for (int j = 1; j < 21; j++)
 		for (int i = 1; i <= n; i++)
-			if (tab[i][j-1] != -1)
-				tab[i][j] = tab[tab[i][j-1]][j-1];
+			tab[i][j] = tab[tab[i][j-1]][j-1];
 }
 
 int lca(int u, int v)
 {
 	if (nivel[u] < nivel[v]) swap(u, v);
 
-	for (int i = maxl-1; i >= 0; i--)
+	for (int i = 20; i >= 0; i--)
 		if (nivel[u]-(1<<i) >= nivel[v])
 			u = tab[u][i];
 
 	if (u == v) return u;
 
-	for (int i = maxl-1; i >= 0; i--)
-		if (tab[u][i] != -1 && tab[v][i] != -1 && tab[u][i] != tab[v][i])
+	for (int i = 20; i >= 0; i--)
+		if (tab[u][i] && tab[v][i] && tab[u][i] != tab[v][i])
 			u = tab[u][i], v = tab[v][i];
 
-	return pai[u];
+	return tab[u][0];
+}
+
+void dfs2(int u, int p)
+{
+	version[u] = new Node();
+	upd(version[p], version[u], 1, maxn-1, a[u], 1);
+
+	for (auto v: grafo[u])
+		if (v != p)
+			dfs2(v, u);
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(false); cin.tie(0);
-
-	int m;
-	cin >> n >> m;
+	scanf("%d %d", &n, &q);
 
 	for (int i = 1; i <= n; i++)
-		cin >> num[i];
+		scanf("%d", &a[i]);
 
-	for (int i = 1; i <= n-1; i++)
+	for (int i = 1; i < n; i++)
 	{
 		int u, v;
-		cin >> u >> v;
+		scanf("%d %d", &u, &v);
 
 		grafo[u].push_back(v); grafo[v].push_back(u);
 	}
 
-	compress();
-
 	version[0] = new Node();
-	build(version[0], 1, n);
+	build(version[0], 1, maxn-1);
 
 	dfs(1, 0);
-	build_lca();
+	dfs2(1, 0);
+	build_tab();
 
-	for (int i = 1; i <= m; i++)
+	for (int i = 1; i <= q; i++)
 	{
 		int u, v, k;
-		cin >> k >> u >> v;
+		scanf("%d %d %d", &k, &u, &v);
 
-		int low = lca(u, v);
-		cout << back[kth(version[ind[u]], version[ind[v]], version[ind[pai[low]]], 1, n, k, num[low])] << "\n";
+		int l = lca(u, v);
+
+		printf("%d\n", kth(version[u], version[v], version[l], 1, maxn-1, k, a[l]));
 	}
 }
