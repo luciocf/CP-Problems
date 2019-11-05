8c45ca0584c3c665bd9dd6c978eba73cdea70370
Lúcio Cardoso <lucio.code@gmail.com>
2019-11-05 14:41:40 -0300

    Added tribo

--- Seletiva IOI/Seletiva 2017/tribo.cpp
+++ Seletiva IOI/Seletiva 2017/tribo.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-const int maxn = 1e4+10;
-
-const int maxk = 55;
-
-const int inf = 1e9+10;
-
 typedef pair<int, int> pii;
 
-int dp[maxn][maxk], aux[maxn][maxk], k;
+const int maxn = 1e4+10;
+const int maxk = 55;
+const int inf = 1e9+10;
+
+int n, k;
+
+int dp[maxn][maxk];
 
 vector<pii> grafo[maxn];
 
@@ -24,37 +24,28 @@ void solve(int u, int p)
 
 	for (auto pp: grafo[u])
 	{
-		int v = pp.first, d = pp.second;
+		int v = pp.first, w = pp.second;
 		if (v == p) continue;
 
 		solve(v, u);
 
-		for (int i = 2; i <= k; i++)
-			aux[u][i] = inf;
-
-		for (int i = 2; i <= k; i++)
+		for (int i = k; i >= 2; i--)
 			for (int j = 1; j < i; j++)
-				aux[u][i] = min(aux[u][i], d+dp[v][j]+dp[u][i-j]);
-
-		for (int i = 2; i <= k; i++)
-			dp[u][i] = min(dp[u][i], aux[u][i]);
+				dp[u][i] = min(dp[u][i], dp[v][j]+w+dp[u][i-j]);
 	}
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(false); cin.tie(0);
-	
-	int n;
-	cin >> n >> k;
+	scanf("%d %d", &n, &k);
 
-	for (int i = 1; i <= n-1; i++)
+	for (int i = 1; i < n; i++)
 	{
-		int u, v, d;
-		cin >> u >> v >> d;
+		int u, v, w;
+		scanf("%d %d %d", &u, &v, &w);
 
-		grafo[u].push_back({v, d});
-		grafo[v].push_back({u, d});
+		grafo[u].push_back({v, w});
+		grafo[v].push_back({u, w});
 	}
 
 	solve(1, 0);
@@ -63,5 +54,5 @@ int main(void)
 	for (int i = 1; i <= n; i++)
 		ans = min(ans, dp[i][k]);
 
-	cout << ans << "\n";
+	printf("%d\n", ans);
 }
