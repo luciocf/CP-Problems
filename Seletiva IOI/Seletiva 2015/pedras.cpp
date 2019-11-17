418bcbe6203786c7b80a1cc56c42788715de2a80
Lúcio Cardoso <lucio.code@gmail.com>
2019-11-17 20:27:53 -0300

    Added pedras

--- Seletiva IOI/Seletiva 2015/pedras.cpp
+++ Seletiva IOI/Seletiva 2015/pedras.cpp
@@ -1,94 +1,96 @@
-// Seletiva IOI 2015 - Pedras (versão online)
+// Seletiva IOI 2015 - Pedras
 // Lúcio Cardoso
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-const int MAXN = 1e5+10;
-const int MAXR = 320;
+const int maxn = 1e5+10;
+const int bucket = 320;
 
-int num[MAXN], b[MAXR][MAXR], pref[MAXN][MAXR], freq[MAXN][MAXR], n, q, s;
-int resp[MAXN];
-
-void pre(void)
+struct Q
 {
-	for (int i = 1; i <= n/s; i++)
-	{
-		for (int j = 0; j+i <= n/s; j++)
-		{
-			int x = j+i;
-			int ans = b[j][x-1], aux;
-			for (int k = x*s; k < min(n, (x+1)*s); k++)
-			{
-				if (!j) aux = pref[num[k]][x];
-				else aux = pref[num[k]][x]-pref[num[k]][j-1];
-				ans = max(ans, aux);
-			}
-			b[j][x] = ans;
-		}
-	}
+	int l, r, bloco, id;
+} q[maxn];
+
+int a[maxn];
+
+int freq[maxn], qtd[maxn], ans[maxn];
+
+bool comp(Q a, Q b)
+{
+	if (a.bloco == b.bloco) return a.r < b.r;
+	return a.bloco < b.bloco;
+}
+
+void add(int i, int &mx)
+{
+	qtd[freq[a[i]]]--;
+
+	++freq[a[i]];
+
+	qtd[freq[a[i]]]++;
+
+	mx = max(mx, freq[a[i]]);
+}
+
+void rem(int i, int &mx)
+{
+	qtd[freq[a[i]]]--;
+
+	--freq[a[i]];
+
+	qtd[freq[a[i]]]++;
+
+	if (freq[a[i]]+1 == mx && qtd[freq[a[i]]+1] == 0)
+		mx--;
 }
 
 int main(void)
 {
-	scanf("%d %d", &n, &q);
-	s = sqrt(n);
+	int n, m;
+	scanf("%d %d", &n, &m);
 
-	map<int, int> M;
-	vector<int> v;
-	for (int i = 0; i < n; i++)
+	map<int, int> mp;
+
+	for (int i = 1; i <= n; i++)
 	{
-		scanf("%d", &num[i]);
-		v.push_back(num[i]);
+		scanf("%d", &a[i]);
+
+		mp[a[i]] = 0;
 	}
 
-	sort(v.begin(), v.end());
+	int aux = 0;
+	for (auto &x: mp)
+		x.second = ++aux;
 
-	int count = -1;
-	for (int i = 0; i < v.size(); i++)
-		if (M.find(v[i]) == M.end()) M[v[i]] = ++count;
+	for (int i = 1; i <= n; i++)
+		a[i] = mp[a[i]];
 
-	for (int i = 0; i < n; i++)
-	{
-		num[i] = M[num[i]];
-		freq[num[i]][i/s]++;
-		b[i/s][i/s] = max(b[i/s][i/s], freq[num[i]][i/s]);
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		pref[i][0] = freq[i][0];
-		for (int j = 1; j <= n/s; j++)
-			pref[i][j] = pref[i][j-1]+freq[i][j];
-	}
-	pre();
-
-	while (q--)
+	for (int i = 1; i <= m; i++)
 	{
 		int l, r;
 		scanf("%d %d", &l, &r);
-		l--, r--;
 
-		int k = l/s, p = r/s;
-
-		if (k == p)
-		{
-			int ans = 0;
-
-			for (int i = l; i <= r; i++) resp[num[i]] = 0;
-			for (int i = l; i <= r; i++) resp[num[i]]++, ans = max(ans, resp[num[i]]);
-			printf("%d\n", ans);
-			continue;
-		}
-		int ans = b[k+1][p-1];
-
-		for (int i = l; i < (k+1)*s; i++) resp[num[i]] = pref[num[i]][p-1]-pref[num[i]][k];
-		for (int i = p*s; i <= r; i++) resp[num[i]] = pref[num[i]][p-1]-pref[num[i]][k];
-
-		for (int i = l; i < (k+1)*s; i++) resp[num[i]]++, ans = max(ans, resp[num[i]]);
-		for (int i = p*s; i <= r; i++) resp[num[i]]++, ans = max(ans, resp[num[i]]);
-
-		printf("%d\n", ans);
+		q[i] = {l, r, l/bucket, i};
 	}
+
+	sort(q+1, q+m+1, comp);
+
+	int l = 1, r = 1, mx = 1;
+	freq[a[1]]++, qtd[1]++;
+
+	for (int i = 1; i <= m; i++)
+	{
+		while (r > q[i].r) rem(r--, mx);
+		while (r < q[i].r) add(++r, mx);
+
+		while (l > q[i].l) add(--l, mx);
+		while (l < q[i].l) rem(l++, mx);
+
+		ans[q[i].id] = mx;
+	}
+
+	for (int i = 1; i <= m; i++)
+		printf("%d\n", ans[i]);
 }
