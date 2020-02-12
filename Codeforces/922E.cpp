cce7440a09d1e85063c9973d433407e4ca1d952a
Lúcio Cardoso <lucio.code@gmail.com>
2020-02-11 23:57:55 -0300

    Added 922E

--- Codeforces/922E.cpp
+++ Codeforces/922E.cpp
@@ -8,8 +8,9 @@ using namespace std;
 typedef long long ll;
 
 const int maxn = 2e4+10;
+const ll inf = 1e18;
 
-ll dp[1010][maxn];
+ll dp[maxn];
 
 int c[maxn];
 int cost[maxn];
@@ -25,19 +26,20 @@ int main(void)
 	for (int i = 1; i <= n; i++)
 		scanf("%d", &cost[i]);
 
-	memset(dp, -1, sizeof dp);
+	for (int i = 1; i < maxn; i++)
+		dp[i] = -inf;
 
-	dp[1][0] = W;
+	dp[0] = W;
 
 	for (int i = 1; i <= n; i++)
 		for (int j = 10000; j >= 0; j--)
-			for (int k = 0; k <= c[i]; k++)
-				if (dp[i][j] - 1ll*k*cost[i] >= 0)
-					dp[i+1][j+k] = max(dp[i+1][j+k], min(1ll*B*(j+k) + 1ll*W, 1ll*X + dp[i][j] - 1ll*cost[i]*k));
+			for (int k = c[i]; k >= 0; k--)
+				if (dp[j] - 1ll*k*cost[i] >= 0)
+					dp[j+k] = max(dp[j+k], min(1ll*B*(j+k) + 1ll*W, 1ll*X + dp[j] - 1ll*cost[i]*k));
 
 	for (int i = 10000; i >= 0; i--)
 	{
-		if (dp[n+1][i] >= 0)
+		if (dp[i] >= 0)
 		{
 			printf("%d\n", i);
 			return 0;
