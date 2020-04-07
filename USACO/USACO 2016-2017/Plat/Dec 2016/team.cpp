b8f1138524387f6d748796ebbc8dbe1f64fc2142
Lúcio Cardoso <lucio.code@gmail.com>
2020-04-07 20:15:02 -0300

    Added team

--- USACO/USACO 2016-2017/Plat/Dec 2016/team.cpp
+++ USACO/USACO 2016-2017/Plat/Dec 2016/team.cpp
@@ -11,12 +11,12 @@ const int mod = 1e9+9;
 int n, m, k;
 int a[maxn], b[maxn];
 
-int dp[maxn][maxn][12], pref[maxn][maxn][12]; 
+int dp[maxn][maxn][11];
+int pref[maxn][maxn][11];
 
 int main(void)
 {
-	freopen("team.in", "r", stdin);
-	freopen("team.out", "w", stdout);
+	freopen("team.in", "r", stdin); freopen("team.out", "w", stdout);
 
 	scanf("%d %d %d", &n, &m, &k);
 
@@ -27,33 +27,33 @@ int main(void)
 		scanf("%d", &b[i]);
 
 	sort(a+1, a+n+1); sort(b+1, b+m+1);
-		
+
 	for (int i = 0; i <= n; i++)
 		for (int j = 0; j <= m; j++)
 			pref[i][j][0] = 1;
 
-	for (int t = 1; t <= k; t++)
+	for (int l = 1; l <= k; l++)
 	{
-		for (int l = 1; l <= n; l++)
+		for (int i = 1; i <= n; i++)
 		{
-			for (int r = 1; r <= m; r++)
+			for (int j = 1; j <= m; j++)
 			{
-				if (a[l] > b[r])
-					dp[l][r][t] = pref[l-1][r-1][t-1];
+				if (a[i] > b[j])
+					dp[i][j][l] = pref[i-1][j-1][l-1];
 
-				pref[l][r][t] = (dp[l][r][t] + pref[l][r-1][t])%mod;
+				pref[i][j][l] = (pref[i][j-1][l] + dp[i][j][l])%mod;
 			}
-
-			for (int r = 1; r <= m; r++)
-				pref[l][r][t] = (pref[l][r][t]+pref[l-1][r][t])%mod;
 		}
+
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= m; j++)
+				pref[i][j][l] = (pref[i][j][l] + pref[i-1][j][l])%mod;
 	}
 
 	int ans = 0;
-
-	for (int l = 1; l <= n; l++)
-		for (int r = 1; r <= m; r++)
-			ans = (ans+dp[l][r][k])%mod;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			ans = (ans + dp[i][j][k])%mod;
 
 	printf("%d\n", ans);
 }
