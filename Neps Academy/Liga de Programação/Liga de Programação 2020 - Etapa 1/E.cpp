d61fcb4ffce9ffb8c8ee3bdda15e779d1763b6c0
Lúcio Cardoso <lucio.code@gmail.com>
2020-04-12 23:37:12 -0300

    Added E

---
+++ Neps Academy/Liga de Programação/Liga de Programação 2020 - Etapa 1/E.cpp
@@ -0,0 +1,14 @@
+// Neps Academy - Liga de Programação 2020 - Etapa 1 - Liga de Programação do Neps
+// Lúcio Cardoso
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	int x;
+	scanf("%d", &x);
+
+	printf("%d\n", (x-9+24)%24);
+}
