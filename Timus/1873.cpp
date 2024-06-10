da4586e3cd688972932dda93e8a21680860b7d9b
Lúcio Cardoso <lucio.code@gmail.com>
2024-06-10 12:51:28 -0300

    Added 1873

---
+++ Timus/1873.cpp
@@ -0,0 +1,40 @@
+// Timus 1873 - GOV Chronicles
+// Lucio Figueiredo
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <cstring>
+#include <set>
+#include <map>
+
+#define ff first
+#define ss second
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	if (n == 0) cout << "5\n";
+	if (n == 1) cout << "21\n";
+	if (n == 2) cout << "12\n";
+	if (n == 3) cout << "2\n";
+	if (n == 4) cout << "1\n";
+	if (n == 5) cout << "4\n";
+	if (n == 6) cout << "6\n";
+	if (n == 7) cout << "1\n";
+	if (n == 8) cout << "4\n";
+	if (n == 9) cout << "4\n";
+	if (n == 10) cout << "1\n";
+	if (n == 11) cout << "0\n";
+	if (n == 12) cout << "1\n";
+	if (n == 13) cout << "1\n";
+}
