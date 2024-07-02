b265c4d243d7e5045026966288a368c10960ac15
Lúcio Cardoso <lucio.code@gmail.com>
2024-07-02 18:38:30 -0300

    Added 1358C

---
+++ Codeforces/1358C.cpp
@@ -0,0 +1,52 @@
+// Codeforces 1358C - Celex Update
+// Lucio Figueiredo
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <set>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+#define ff first
+#define ss second
+#define mkp make_pair
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const char nl = '\n';
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+void solve()
+{
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << 1ll*(x2-x1)*(y2-y1)+1ll << nl;
+}
+
+int main(void)
+{
+	cin.tie(0)->sync_with_stdio(0); 
+    cin.exceptions(cin.failbit);
+    // xor ^ 
+    
+	int tc = 1;
+	cin >> tc;
+	while (tc--)
+	{
+		solve();
+	}
+}
+
