8b71a3997cbcb8c076a882da77c15aeee946f555
Lúcio Cardoso <lucio.code@gmail.com>
2024-07-12 15:14:00 -0300

    Added sums

--- POI/POI 2010/sums.cpp
+++ POI/POI 2010/sums.cpp
@@ -3,80 +3,101 @@
 
 // Solution: https://github.com/mostafa-saad/MyCompetitiveProgramming/blob/master/Olympiad/POI/POI10-sums-Mohamed_Nasser.pdf
 
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <set>
+#include <map>
+#include <cmath>
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<ll, int> pii;
+#define ff first
+#define ss second
+#define mkp make_pair
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
 
-const int maxn = 5e4+10;
-const ll inf = 1e18+10;
+const char nl = '\n';
+
+typedef pair<int, int> pii;
+typedef long long ll;
+
+const int maxn = 5e3+10;
+const int maxv = 5e4+10;
+const int inf = 1e9+10;
 
 int n;
 int a[maxn];
-
-ll dist[maxn];
-
-bool mark[maxn];
+int dist[maxv];
+bool mark[maxv];
 
 void dijkstra(void)
 {
-	for (int i = 1; i < maxn; i++)
-		dist[i] = inf;
+    dist[0] = 0;
+    for (int i = 1; i < maxv; i++)
+        dist[i] = inf;
 
-	priority_queue<pii, vector<pii>, greater<pii>> fila;
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    pq.push({0, 0});
+    
+    while (!pq.empty())
+    {
+        auto [d, u] = pq.top(); pq.pop();
+        if (mark[u]) continue;
+        mark[u] = 1;
 
-	fila.push({0, 0}), dist[0] = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            int v = (u+a[i])%a[1];
 
-	while (!fila.empty())
-	{
-		int u = fila.top().second; fila.pop();
+            if (dist[v] > d+a[i])
+            {
+                dist[v] = d+a[i];
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
 
-		if (mark[u]) continue;
-		mark[u] = 1;
+void solve()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
 
-		for (int i = 1; i <= n; i++)
-		{
-			int v = (a[i]+u)%a[1];
-			int w = a[i];
+    dijkstra();
 
-			if (dist[v] > dist[u]+1ll*w)
-			{
-				dist[v] = dist[u]+1ll*w;
-				fila.push({dist[v], v});
-			}
-		}
-	}
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int x;
+        cin >> x;
+
+        if (dist[x%a[1]] <= x) cout << "TAK\n";
+        else cout << "NIE\n";
+    }
 }
 
 int main(void)
 {
-	scanf("%d", &n);
-
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &a[i]);
-
-	dijkstra();
-
-	int m;
-	scanf("%d", &m);
-
-	for (int i = 1; i <= m; i++)
+	cin.tie(0)->sync_with_stdio(0); 
+    cin.exceptions(cin.failbit);
+    // xor ^ 
+    
+	int tc = 1;
+	while (tc--)
 	{
-		ll x;
-		scanf("%lld", &x);
-
-		if (dist[x%a[1]] == inf)
-		{
-			printf("NIE\n");
-			continue;
-		}
-
-		if (dist[x%a[1]] <= x)
-			printf("TAK\n");
-		else
-			printf("NIE\n");
+		solve();
 	}
 }
+
